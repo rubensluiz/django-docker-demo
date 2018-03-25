fb62dc5353c0590d6f8073b975595c0589b8b41b
Rubens Luiz <rles@Rubenss-MacBook-Pro.local>
2018-03-25 01:42:37 -0300

    merge

--- src/demoapp/views.py
+++ src/demoapp/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 import socket
 
 def index(request):
-    return HttpResponse("Hello, world! 2.0\n")
+    hostname = socket.gethostname()
+    return HttpResponse(hostname + " - Hello, world! \n")
