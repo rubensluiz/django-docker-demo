c9132498f5d6a1b9ef4f71e70933c3a08340b6ce
Rubens Luiz <rubens.luiz@gmail.com>
2019-02-22 12:10:46 +0100

    Update views.py

--- src/demoapp/views.py
+++ src/demoapp/views.py
@@ -3,4 +3,4 @@ import socket
 
 def index(request):
     hostname = socket.gethostname()
-    return HttpResponse(hostname + " - Hello, world! \n")
+    return HttpResponse(hostname + " - Hello, world! v1.0\n")
